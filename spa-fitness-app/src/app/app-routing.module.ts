db730c881099917e5a0d3a43efbfa368071c373b
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-11 19:16:22 +0200

    Rutas

--- spa-fitness-app/src/app/app-routing.module.ts
+++ spa-fitness-app/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [];
 
