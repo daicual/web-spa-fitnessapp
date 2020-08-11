60bcddffbf99caf2c6e04a32458b30e89531a440
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-11 21:53:21 +0200

    Registro (fallan los imports)
    No funciona, fallan los imports y no consigue acceder a las librerias de firebase

--- spa-fitness-app/src/app/components/dashboard/dashboard.component.ts
+++ spa-fitness-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+//FIREBASE
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from '@angular/fire/auth';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
+const formRegistro = document.getElementById('formRegistro');
+formRegistro.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = <HTMLInputElement>document.getElementById('registroEmail');
+  const password1 = <HTMLInputElement>(
+    document.getElementById('registroPassword1')
+  );
+  const password2 = <HTMLInputElement>(
+    document.getElementById('registroPassword2')
+  );
+  var registroValido = true;
+  if (password1.value != password2.value) {
+    registroValido = false;
   }
 
-}
+  //Si los datos del registro son válidos, llamamos al servidor para completarlo
+  if (registroValido) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email.value, password1.value)
+      .then((userCredential) => {
+        console.log('registro');
+      });
+  }
+});
