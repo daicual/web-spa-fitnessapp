4feb463a55c702dfd616f35b5409b533b4039ecd
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-11 20:00:33 +0200

    Solución 2 Dashboard encima de router
    Dashboard encima de router
    HomeComponent como ruta por defecto

--- spa-fitness-app/src/app/app.module.ts
+++ spa-fitness-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
