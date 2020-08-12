a2776c1b629e73b83b24f5c1c825e1e6d3b00542
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-12 21:26:22 +0200

    Pasar formularios al nuevo componente

--- spa-fitness-app/src/app/components/dashboard/dashboard.component.ts
+++ spa-fitness-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,79 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth/auth.service';
-//FIREBASE
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import {
-  FormGroup,
-  FormBuilder,
-  FormControl,
-  Validators,
-} from '@angular/forms';
-import { MustMatch } from 'src/app/_helpers/form-helper';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  formRegistro: FormGroup;
-  formInicio: FormGroup;
   formBotones: FormGroup;
-  constructor(private _builder: FormBuilder, private authSvc: AuthService) {
-    this.formRegistro = this._builder.group(
-      {
-        email: [
-          '',
-          Validators.compose([Validators.email, Validators.required]),
-        ],
-        password1: ['', Validators.required],
-        password2: ['', Validators.required],
-      },
-      {
-        validator: MustMatch('password1', 'password2'),
-      }
-    );
-    this.formInicio = this._builder.group({
-      email: ['', Validators.compose([Validators.email, Validators.required])],
-      password: ['', Validators.required],
-    });
+  constructor(private _builder: FormBuilder) {
     this.formBotones = this._builder.group({});
   }
 
-  registrarEmailPassword(values) {
-    this.authSvc.registro(values.email, values.password1);
-  }
-  inicioEmailPassword(values) {
-    this.authSvc.inicio(values.email, values.password);
-    console.log(values);
-  }
   ngOnInit(): void {}
 }
-
-/**
-const formRegistro = document.getElementById('formRegistro');
-formRegistro.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = <HTMLInputElement>document.getElementById('registroEmail');
-  const password1 = <HTMLInputElement>(
-    document.getElementById('registroPassword1')
-  );
-  const password2 = <HTMLInputElement>(
-    document.getElementById('registroPassword2')
-  );
-  var registroValido = true;
-  if (password1.value != password2.value) {
-    registroValido = false;
-  }
-
-  //Si los datos del registro son válidos, llamamos al servidor para completarlo
-  if (registroValido) {
-    this.afAuth
-      .auth()
-      .createUserWithEmailAndPassword(email.value, password1.value)
-      .then((userCredential) => {
-        console.log('registro');
-      });
-  }
-});
-*/
--- spa-fitness-app/src/app/components/login/login.component.ts
+++ spa-fitness-app/src/app/components/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MustMatch } from 'src/app/_helpers/form-helper';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  formRegistro: FormGroup;
+  formInicio: FormGroup;
+  constructor(private _builder: FormBuilder, private authSvc: AuthService) {
+    this.formRegistro = this._builder.group(
+      {
+        email: [
+          '',
+          Validators.compose([Validators.email, Validators.required]),
+        ],
+        password1: ['', Validators.required],
+        password2: ['', Validators.required],
+      },
+      {
+        validator: MustMatch('password1', 'password2'),
+      }
+    );
+    this.formInicio = this._builder.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  registrarEmailPassword(values) {
+    this.authSvc.registro(values.email, values.password1);
+  }
+  inicioEmailPassword(values) {
+    this.authSvc.inicio(values.email, values.password);
+    console.log(values);
+  }
 }
