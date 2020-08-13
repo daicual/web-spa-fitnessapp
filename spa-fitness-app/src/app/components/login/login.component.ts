848ce631d1a36961d12f939b090d6be1f4ded72f
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-13 11:53:19 +0200

    Crear cambio entre registro e inicio

--- spa-fitness-app/src/app/components/login/login.component.ts
+++ spa-fitness-app/src/app/components/login/login.component.ts
@@ -41,4 +41,7 @@ export class LoginComponent implements OnInit {
     this.authSvc.inicio(values.email, values.password);
     console.log(values);
   }
+  swapInicioRegistro() {
+    this.inicio = !this.inicio;
+  }
 }
