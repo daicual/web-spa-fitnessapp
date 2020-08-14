5734ae05dd61261e5c8733e071ca5bfec025b588
Darío Aícua Ubierna <dariolaredo@gmail.com>
2020-08-14 16:29:27 +0200

    Recoger datos usuario

--- spa-fitness-app/src/app/components/user/user.component.ts
+++ spa-fitness-app/src/app/components/user/user.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  public user$: Observable<any> = this.authSvc.afAuth.user;
+  constructor(private authSvc: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
